e51b0ad6dce9470eb643bd5c50748da5987bdd63
Sathwik Chiluveru <51926084+SathwikChiluveru@users.noreply.github.com>
2023-09-25 21:44:03 +0800

    Header created

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,14 +1,12 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Header from './components/Header'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
+        <Header />
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
