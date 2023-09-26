a8b6756705259388ce651ee1c8a7049cfdb98411
Sathwik Chiluveru <51926084+SathwikChiluveru@users.noreply.github.com>
2023-09-26 22:05:01 +0800

    Contact Component Done

---
+++ frontend/src/app/components/Contact.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import '../styles/ContactComponent.css'
+// import displayImg from '../images/background.jpg'
+export default function Contact(){
+  return (
+    <div className='flex-container'>
+        <div className='picture-display'>
+            <img src="background.jpg" alt='why'/>
+        </div>
+        <div>
+            <form>
+                <h1>Contact Us</h1>
+                <p className='title1'>Need to get in touch with us?</p>
+                <p className='title2'>Fill out the form with your inquiry</p>
+                <input type='text' name='fname' id='' placeholder='First Name'/>
+                <input type='text' name='lname' id='' placeholder='Last Name'/>
+                <input type='email' name='email' id='' placeholder='Enter Email'/>
+                <textarea type='text' id='message' cols={38} rows={10} placeholder='Message' />
+                <button type='submit'>Submit</button>
+            </form>
+        </div>
+    </div>
+  )
+}
