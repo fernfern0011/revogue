aeb79c7f323e097e2b598fb6e73f6fa9966ac203
Sathwik Chiluveru <51926084+SathwikChiluveru@users.noreply.github.com>
2023-09-26 22:20:02 +0800

    Footer Component Done

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,12 +1,14 @@
 // import Image from 'next/image'
 // import styles from './page.module.css'
 import  Contact  from './components/Contact'
+import Footer from './components/Footer'
 
 export default function Home() {
   return (
    <main>
       <div className='contact-form'>
         <Contact/>
+        <Footer />
       </div>
     </main>
   )
