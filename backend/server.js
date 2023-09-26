650b7ca22db5c690cd44ba1700cfa70575b261b5
Nang Khong Kham <n.khongkham0011@gmail.com>
2023-09-26 20:30:24 +0800

    Connect DB to Backend

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
-const PORT = 8080;
+require('dotenv').config();
+
+app.use(express.json())
 
 app.get("/api/home", (req, res) => {
     res.json({ message: "Hello World" });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+app.listen(process.env.PORT, () => {
+    console.log(`Server started on port ${process.env.PORT}`);
 })
