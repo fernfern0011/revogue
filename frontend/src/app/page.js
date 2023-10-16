8a7b46534b94fcaefefa87e932e929ba228ece0b
khairyo <ryu.minatozaki8@gmail.com>
2023-10-16 10:38:23 +0800

    commit

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,11 +1,14 @@
 import styles from './page.module.css'
 import Navbar from './components/Navbar'
+import ItemPage from './components/ItemPage'
 
 export default function Home() {
   return (
     <main className={styles.main}>
 
       <Navbar />
+
+      <ItemPage />
 
     </main>
   )
