766623f637349bf59928abd8349adb306b8a8801
Sathwik Chiluveru <51926084+SathwikChiluveru@users.noreply.github.com>
2023-10-22 00:01:37 +0800

    Stripe API endpoint done

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,30 @@ app.use("/api/user-profile", require("./routes/userProfile.route"))
 // Product
 app.use("/api/product", require("./routes/product.route"))
 
+//Stripe
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+app.post('/create-payment-intent', async (req, res) => {
+  const { amount, currency } = req.body;
+
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency,
+    });
+
+    res.json({ clientSecret: paymentIntent.client_secret });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
+});
+
+//TEST
+app.get('/', (req, res) => {
+  res.send('Stripe Payment Backend');
+});
+
 app.listen(process.env.PORT, () => {
     console.log(`Server started on port ${process.env.PORT}`);
 })
