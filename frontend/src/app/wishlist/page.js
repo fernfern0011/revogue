fc7a6b9b904b73d120185c8e0ae9789ac790cd7f
Sathwik Chiluveru <51926084+SathwikChiluveru@users.noreply.github.com>
2023-10-21 20:16:10 +0800

    WishList component and page done

---
+++ frontend/src/app/wishlist/page.js
@@ -0,0 +1,15 @@
+"use client"
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import Wishlist from '../components/WishList'
+
+export default function WishListPage(){
+  return (
+    <div>
+        <Navbar/>
+        <Wishlist/>
+
+    </div>
+  )
+}
