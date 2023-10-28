31d8537074d3d27c58df1eee7629757b6101a849
Nang Khong Kham <n.khongkham0011@gmail.com>
2023-10-29 04:31:28 +0800

    Update Shop page #2

--- frontend/src/app/shop/page.js
+++ frontend/src/app/shop/page.js
@@ -4,35 +4,35 @@ import Sidebar from "../components/Sidebar/Sidebar";
 import { backendUrl } from '../../../config';
 import './page.css'
 import ProductListing from '../components/ProductListing';
-import { Suspense } from 'react';
+// import { Suspense } from 'react';
 
-async function getProductData() {
+// async function getProductData() {
 
-  // get product list
-  const getProductRes = await fetch(`${backendUrl}/api/product/get-all-products`, {
-    headers: { "Content-Type": "application/json" },
-    cache: 'no-cache'
-  })
+//   // get product list
+//   const getProductRes = await fetch(`${backendUrl}/api/product/get-all-products`, {
+//     headers: { "Content-Type": "application/json" },
+//     cache: 'no-cache'
+//   })
 
-  if (!getProductRes.status == 200) {
-    throw new Error('failed to fetch data')
-  }
+//   if (!getProductRes.status == 200) {
+//     throw new Error('failed to fetch data')
+//   }
 
-  return await getProductRes.json()
-}
+//   return await getProductRes.json()
+// }
 
 async function ShopPage() {
-  const productData = await getProductData();
-  const productList = await productData.data;
+  // const productData = await getProductData();
+  // const productList = await productData.data;
 
-  if (productList.length == 0) {
-    return (
-      <div className='relative flex items-center justify-center'>
-        <img src="empty.png" alt="" />
-        <h1 className='absolute top-[80%] text-2xl text-purple-600'>No products</h1>
-      </div>
-    )
-  }
+  // if (productList.length == 0) {
+  //   return (
+  //     <div className='relative flex items-center justify-center'>
+  //       <img src="empty.png" alt="" />
+  //       <h1 className='absolute top-[80%] text-2xl text-purple-600'>No products</h1>
+  //     </div>
+  //   )
+  // }
 
   return (
     <div>
@@ -46,11 +46,11 @@ async function ShopPage() {
         </div>
 
         <div className='d-flex flex-wrap col-lg-9'>
-          <Suspense fallback={<p>Loading...</p>}>
+          {/* <Suspense fallback={<p>Loading...</p>}>
             {productList.map((product) => (
               <ProductListing productid={product.productid} productname={product.productname} price={product.price} image={product.images} />
             ))}
-          </Suspense>
+          </Suspense> */}
         </div>
       </div>
     </div>
