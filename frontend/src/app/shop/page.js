6140708c6bdb68d98b6c459316e98d04d2a17163
Nang Khong Kham <n.khongkham0011@gmail.com>
2023-10-31 06:22:38 +0800

    Update Shop page #6

--- frontend/src/app/shop/page.js
+++ frontend/src/app/shop/page.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import Sidebar from "../components/Sidebar/Sidebar";
 import './page.css'
+import { backendUrl } from '../../../config';
 import ProductListing from '../components/ProductListing';
 import { Suspense } from 'react';
-import { getAllProducts } from '../lib/product/data';
+// import { getAllProducts } from '../lib/product/data';
+
+async function getProductData() {
+  // get product list
+  const getProductRes = await fetch(`${backendUrl}/api/product/get-all-products`, {
+    headers: { "Content-Type": "application/json" }
+  })
+
+  console.log(getProductRes.status);
+  if (!getProductRes.status == 200) {
+    throw new Error('Failed to fetch products')
+  }
+
+  return await getProductRes.json()
+}
 
 async function ShopPage() {
-  const productData = await getAllProducts();
-  console.log(productData);
+  // const productData = await getAllProducts();
+  // console.log(productData);
+
+  const productData = await getProductData();
+  const productList = await productData.data;
+
+  if (productList.length == 0) {
+    return (
+      <div className='relative flex items-center justify-center'>
+        <img src="empty.png" alt="" />
+        <h1 className='absolute top-[80%] text-2xl text-purple-600'>No products</h1>
+      </div>
+    )
+  }
 
   return (
     <div>
@@ -22,7 +49,7 @@ async function ShopPage() {
 
         <div className='d-flex flex-wrap col-lg-9'>
           <Suspense fallback={<p>Loading products...</p>}>
-            {productData.map((product) => (
+            {productList.map((product) => (
               <ProductListing key={product.productid} productid={product.productid} productname={product.productname} price={product.price} image={product.images} />
             ))}
           </Suspense>
