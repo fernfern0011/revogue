c435625aef2b38a9dba9cccea59fc7e2d15fc192
Sathwik Chiluveru <51926084+SathwikChiluveru@users.noreply.github.com>
2023-11-02 05:17:04 +0800

    Stripe Payment Gateway Done

--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,4 @@
 //config.js
-module.exports = { backendUrl: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000' };
+module.exports = { 
+    backendUrl: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000',
+    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY };
