595e3f8477b65803f235aa988f7e5f18b760f57e
Sathwik Chiluveru <51926084+SathwikChiluveru@users.noreply.github.com>
2023-11-03 22:48:15 +0800

    Sale confirmation + cancellatio done

--- frontend/src/app/components/SalesConfirmation.js
+++ frontend/src/app/components/SalesConfirmation.js
@@ -7,8 +7,14 @@ import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
 import '../styles/Order.css'
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 const SalesConfirmation = ({ saleItem }) => {
+
+    const router = useRouter();
+    const [isConfirmed, setIsConfirmed] = useState(false);
+    const [isCancelled, setIsCancelled] = useState(false);
 
     const parsedOrderDetailsArray = saleItem.orderdetails.map((orderDetailString) => {
         try {
@@ -18,6 +24,58 @@ const SalesConfirmation = ({ saleItem }) => {
           return null; // You can return a default value or handle the error differently
         }
     });
+
+    const handleConfirm = async () => {
+      try {
+        const response = await fetch(`https://revogue-backend.vercel.app/api/mysales/confirm`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            orderid: saleItem.orderid,
+            buyerid: saleItem.buyerid,
+            sellerid: saleItem.sellerid,
+          }),
+        });
+  
+        if (response.ok) {
+          setIsConfirmed(true);
+          // Wait for a short delay (e.g., 1000ms) before refreshing the page
+          alert("Sale is Confirmed!")
+        } else {
+          console.error('Failed to confirm the order');
+        }
+      } catch (error) {
+        console.error('Error while confirming order:', error);
+      }
+    };
+
+    const handleCancel = async () => {
+      try {
+        const response = await fetch(`https://revogue-backend.vercel.app/api/mysales/cancel`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            orderid: saleItem.orderid,
+            buyerid: saleItem.buyerid,
+            sellerid: saleItem.sellerid,
+          }),
+        });
+  
+        if (response.ok) {
+          setIsCancelled(true);
+          // Wait for a short delay (e.g., 1000ms) before refreshing the page
+          alert("Sale is Cancelled!")
+        } else {
+          console.error('Failed to cancel the order');
+        }
+      } catch (error) {
+        console.error('Error while cancelling order:', error);
+      }
+    };
 
   return (
     <Col xs="12" sm="6" md="4" className="px-2 pb-3">
@@ -47,10 +105,10 @@ const SalesConfirmation = ({ saleItem }) => {
             </Col>
         </Row>
         <div className="text-center">
-            <Button variant="contained" className="custom-confirm-button mx-2">
+            <Button variant="contained" className="custom-confirm-button mx-2" onClick={handleConfirm} disabled={isConfirmed}>
             Confirm
             </Button>
-            <Button variant="contained" className="custom-cancel-button mx-2">
+            <Button variant="contained" className="custom-cancel-button mx-2" onClick={handleCancel} disabled={isCancelled}>
             Cancel
             </Button>
         </div>
